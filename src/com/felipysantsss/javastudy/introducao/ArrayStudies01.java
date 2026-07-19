4049a6993606578b714377c78f11a7df24d7ba6f
Felipy <tercetime18@gmail.com>
2026-07-19 14:54:52 -0300

    fix: 1. Change loop dependency (quantity) for years.length 2. Add scanner closer

--- src/com/felipysantsss/javastudy/introducao/ArrayStudies01.java
+++ src/com/felipysantsss/javastudy/introducao/ArrayStudies01.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 public class ArrayStudies01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] years = null;
 
         System.out.println("Enter the quantity of years old: ");
         int quantity = input.nextInt();
-        years = new int[quantity];
+        int[] years = new int[quantity];
 
         System.out.println("Ok! Enter " + quantity + " years old: ");
-        for (int i=0; i<quantity; i++){
+        for (int i=0; i<years.length; i++){
             years[i] = input.nextInt();
         }
 
 
         System.out.println("Years old: " + Arrays.toString(years));
+
+        input.close();
     }
 }
 
